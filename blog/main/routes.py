b351258c62573ec60272591580a9b8ac4e56191b
JoannaXu <xutianjiao97@sina.com>
2022-01-04 00:19:44 +0000

    1. create base files (header & footer file) 2. maintain login functionality and add error route

--- blog/main/routes.py
+++ blog/main/routes.py
@@ -29,29 +29,13 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
-            login_user(user, form.remember_me.data)
-            next = request.args.get('next')
-            if next is None or not next.startswith('/'):
-                next = url_for('main.home')
-            return redirect(next)
-        flash('Invalid username or password.')
+                login_user(user, form.remember_me.data)
+                next = request.args.get('next')
+                if next is None or not next.startswith('/'):
+                    next = url_for('main.home')
+                return redirect(next)
+        return redirect(url_for('main.login_error'))
     return render_template('login.html', form=form)
-    #     if user is None:
-    #         session['known'] = False
-    #         session['msg'] = 'Incorrect email or password supplied'
-    #         return redirect(url_for('.home'))
-    #     else:
-    #         if user.password != form.password.data:
-    #             session['known'] = False
-    #             session['msg'] = 'Incorrect email or password supplied'
-    #         else:
-    #             session['known'] = True
-    #             session['username'] = user.username
-    #             return redirect(url_for('.home'))
-    #         form.email.data = ''
-    #         form.password.data = ''
-    # return render_template('login.html',
-    #                        form=form, email=email, password=password)
 
 
 @main.route('/logout')
@@ -64,29 +48,7 @@ def logout():
 
 @main.route('/register', methods=['GET', 'POST'])
 def register():
-    firstname = None
-    lastname = None
-    email = None
-    password = None
-    repPassword = None
     form = RegisterForm()
     if form.validate_on_submit():
-        firstname = form.firstname.data
-        lastname = form.lastname.data
-        email = form.email.data
-        password = form.password.data
-        repPassword = form.repPassword.data
-
-        form.firstname.data = ''
-        form.lastname.data = ''
-        form.email.data = ''
-        form.password.data = ''
-        form.repPassword.data = ''
-    return render_template('register.html',
-                           form=form, firstname=firstname,
-                           lastname=lastname, email=email,
-                           password=password, repPassword=repPassword)
-
-
-
-
+        return None
+    return render_template('register.html', form=form)
